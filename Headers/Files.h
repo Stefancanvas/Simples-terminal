e16a91be3b084c8f28849002ff2438c2d91ca4f6
LucasPB710 <lucasperonicobarbotin@gmail.com>
2020-10-24 19:28:43 -0300

    Adicionando coisa com string

--- Headers/Files.h
+++ Headers/Files.h
@@ -6,3 +6,8 @@ namespace Files{
   void criarArq(std::string nome);
   void read(std::string nome);
 }
+
+namespace Str{
+  std::string reverse(std::string inp); //reverte
+  std::string remove(char ch, std::string inp); //remove todos os char da string inp
+}
